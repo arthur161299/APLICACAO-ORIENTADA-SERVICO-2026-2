8edd6bcf022bc0dfe3becc8af9d73fd603b86881
Aluno <Aluno@Inovacao-080922.unicap.br>
2026-08-27 19:21:06 -0300

    novas mudanças

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,21 +1,24 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
+  import 'dotenv/config';
+  import cors from 'cors';
+  import express from 'express';
 
-const app = express();
+  const app = express();
 
-app.use(cors());
+  app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  // Esse link leva para a página /hello
+  res.send('<h1>Hello World!</h1><a href="/hello">Ir para Olá, Turma!</a>');
 });
 
 app.get('/hello', (req, res) => {
-  res.send('Olá, Turma!');
+  // Esse link volta para a página raiz /
+  res.send('<h1>Olá, Turma!</h1><a href="/">Voltar para o Início</a>');
 });
 
-const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`),
-);
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () =>
+    console.log(`Example app listening on port ${port}!`),
+  );
